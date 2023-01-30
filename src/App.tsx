d517db4f875fd20a39aab7d582e07322331d6cb6
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2023-01-30 07:50:31 -0300

    main.tsx and app tsx basics

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+
+function App() {
+
+  return (
+    <div className="app">
+
+
+
+      <div className='aside'>
+      </div>
+
+      <main>
+      </main>
+
+    </div>
+  )
+}
+
+export default App
